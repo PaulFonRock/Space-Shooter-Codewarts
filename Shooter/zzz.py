6cf8d702e45697fd7b725afda5f033500b974c65
PaulFonRock <100071152+PaulFonRock@users.noreply.github.com>
2022-02-20 13:46:10 +0300

    Add files via upload

---
+++ Shooter/zzz.py
@@ -0,0 +1,5 @@
+types_list = ['bolt', 'pill', 'shield', 'star', 'things']
+image_name_list = ["bolt_gold.png", "pill_gold.png", "shield_gold.png",
+                       "star_gold.png", "things_gold.png"]
+powerups_images = dict(zip(types_list, image_name_list))
+print(powerups_images)
